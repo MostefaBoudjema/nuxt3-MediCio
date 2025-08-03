51b1fd6c75a8481802f9c09ed871fa24d4e4154a
mostfa <myreal17@gmail.com>
2025-08-03 19:26:33 +0100

    update footer

--- app/components/shared/FooterSection.vue
+++ app/components/shared/FooterSection.vue
@@ -3,10 +3,11 @@
 </script>
 <template>
     <footer id="footer" class="footer light-background">
+
         <div class="container footer-top">
             <div class="row gy-4">
                 <div class="col-lg-4 col-md-6 footer-about">
-                    <NuxtLink to="/" class="logo d-flex align-items-center">
+                    <NuxtLink href="/" class="logo d-flex align-items-center">
                         <span class="sitename">Medicio</span>
                     </NuxtLink>
                     <div class="footer-contact pt-3">
@@ -22,16 +23,59 @@
                         <a href=""><i class="bi bi-linkedin"></i></a>
                     </div>
                 </div>
-                <!-- ...rest of the footer... -->
+
+                <div class="col-lg-2 col-md-3 footer-links">
+                    <h4>Useful Links</h4>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#about">About us</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#">Terms of service</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-3 footer-links">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li><a href="#">Web Design</a></li>
+                        <li><a href="#">Web Development</a></li>
+                        <li><a href="#">Product Management</a></li>
+                        <li><a href="#">Marketing</a></li>
+                        <li><a href="#">Graphic Design</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-3 footer-links">
+                    <h4>Our Services</h4>
+                    <ul>
+                        <li><a href="#">General Checkups</a></li>
+                        <li><a href="#">Pediatrics</a></li>
+                        <li><a href="#">Cardiology</a></li>
+                        <li><a href="#">Dental Care</a></li>
+                        <li><a href="#">Emergency Services</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-2 col-md-3 footer-links">
+                    <h4>Quick Links</h4>
+                    <ul>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Our Doctors</a></li>
+                        <li><a href="#">Appointments</a></li>
+                        <li><a href="#">Patient Portal</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+
+
             </div>
         </div>
+
         <div class="container copyright text-center mt-4">
-            <p>© <span>Copyright</span> <strong class="px-1 sitename">Medicio</strong> <span>All Rights Reserved</span>
+            <p>© <span>Copyright 2025</span> <strong class="px-1 sitename">Medicio</strong> <span>All Rights Reserved</span>
             </p>
-            <div class="credits">
-                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> Distributed by <a
-                    href="https://themewagon.com">ThemeWagon</a>
-            </div>
         </div>
+
     </footer>
 </template>
