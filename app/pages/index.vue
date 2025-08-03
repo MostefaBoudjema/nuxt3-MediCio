7abf4bcc9e7cd5ad2c912f4faad7bad29ce37429
mostfa <myreal17@gmail.com>
2025-08-03 19:27:06 +0100

    delete wrong img link

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -34,7 +34,6 @@ useHead({
     { name: 'keywords', content: '' }
   ],
   link: [
-    // { rel: 'icon', href: '/favicon.png' },
     { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
     { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap' },
     { rel: 'stylesheet', href: '/vendor/bootstrap/css/bootstrap.min.css' },
@@ -51,7 +50,7 @@ useHead({
     { src: '/vendor/aos/aos.js', body: true },
     { src: '/vendor/glightbox/js/glightbox.min.js', body: true },
     { src: '/vendor/purecounter/purecounter_vanilla.js', body: true },
-    { src: '/vendor/swiper/swiper-bundle.min.js', body: true },
+    // { src: '/vendor/swiper/swiper-bundle.min.js', body: true },
     { src: '/js/main.js', body: true }
   ]
 })
