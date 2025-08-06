07da41f14116c887980a7c0455f99297f178458c
mostfa <myreal17@gmail.com>
2025-08-06 02:58:18 +0100

    add aos

--- layouts/default.vue
+++ layouts/default.vue
@@ -39,4 +39,13 @@ useHead({
     { src: '/js/main.js', body: true }
   ]
 })
+
+onMounted(() => {
+  AOS.init({
+    duration: 1000, // Animation duration
+    easing: 'ease-in-out', // Animation easing
+    once: true, // Whether animation should happen only once
+    mirror: false, // Whether elements should animate out while scrolling past them
+  });
+});
 </script>
