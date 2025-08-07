f567dc9f7b5ee7eb0390d47aeac6a5f0ba53ec28
mostfa <myreal17@gmail.com>
2025-08-07 13:51:44 +0100

    clean code

--- public/js/main.js
+++ public/js/main.js
@@ -12,43 +12,43 @@
   /**
    * Apply .scrolled class to the body as the page is scrolled down
    */
-  function toggleScrolled() {
-    const selectBody = document.querySelector('body');
-    const selectHeader = document.querySelector('#header');
-    if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
-    window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
-  }
+  // function toggleScrolled() {
+  //   const selectBody = document.querySelector('body');
+  //   const selectHeader = document.querySelector('#header');
+  //   if (!selectHeader.classList.contains('scroll-up-sticky') && !selectHeader.classList.contains('sticky-top') && !selectHeader.classList.contains('fixed-top')) return;
+  //   window.scrollY > 100 ? selectBody.classList.add('scrolled') : selectBody.classList.remove('scrolled');
+  // }
 
-  document.addEventListener('scroll', toggleScrolled);
-  window.addEventListener('load', toggleScrolled);
+  // document.addEventListener('scroll', toggleScrolled);
+  // window.addEventListener('load', toggleScrolled);
 
   /**
    * Mobile nav toggle
    */
-  const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
+  // const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
-  function mobileNavToogle() {
-    document.querySelector('body').classList.toggle('mobile-nav-active');
-    mobileNavToggleBtn.classList.toggle('bi-list');
-    mobileNavToggleBtn.classList.toggle('bi-x');
-  }
-  if (mobileNavToggleBtn) {
-    mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
-  }
+  // function mobileNavToogle() {
+  //   document.querySelector('body').classList.toggle('mobile-nav-active');
+  //   mobileNavToggleBtn.classList.toggle('bi-list');
+  //   mobileNavToggleBtn.classList.toggle('bi-x');
+  // }
+  // if (mobileNavToggleBtn) {
+  //   mobileNavToggleBtn.addEventListener('click', mobileNavToogle);
+  // }
 
   /**
    * Hide mobile nav on same-page/hash links
    */
-  const navmenuLinks = document.querySelectorAll('#navmenu a');
-  if (navmenuLinks.length) {
-    navmenuLinks.forEach(navmenu => {
-      navmenu.addEventListener('click', () => {
-        if (document.querySelector('.mobile-nav-active')) {
-          mobileNavToogle();
-        }
-      });
-    });
-  }
+  // const navmenuLinks = document.querySelectorAll('#navmenu a');
+  // if (navmenuLinks.length) {
+  //   navmenuLinks.forEach(navmenu => {
+  //     navmenu.addEventListener('click', () => {
+  //       if (document.querySelector('.mobile-nav-active')) {
+  //         mobileNavToogle();
+  //       }
+  //     });
+  //   });
+  // }
 
   /**
    * Toggle mobile nav dropdowns
@@ -65,28 +65,20 @@
     });
   }
 
-  /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
+
 
   /**
    * Animation on scroll function and init
    */
-  function aosInit() {
-    AOS.init({
-      duration: 600,
-      easing: 'ease-in-out',
-      once: true,
-      mirror: false
-    });
-  }
-  window.addEventListener('load', aosInit);
+  // function aosInit() {
+  //   AOS.init({
+  //     duration: 600,
+  //     easing: 'ease-in-out',
+  //     once: true,
+  //     mirror: false
+  //   });
+  // }
+  // window.addEventListener('load', aosInit);
 
   /**
    * Auto generate the carousel indicators
@@ -139,17 +131,6 @@
 
   // window.addEventListener("load", initSwiper);
 
-  /**
-   * Frequently Asked Questions Toggle
-   */
-  const faqItems = document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle');
-  if (faqItems.length) {
-    faqItems.forEach((faqItem) => {
-      faqItem.addEventListener('click', () => {
-        faqItem.parentNode.classList.toggle('faq-active');
-      });
-    });
-  }
 
   /**
    * Correct scrolling position upon page load for URLs containing hash links.
